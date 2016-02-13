7873f5fe44e02ef843e7fc39c72d150b9ca6e514
Tim Rodger <tim.rodger@gmail.com>
2016-02-13 09:31:35 +0000

    Runs npm update, removes lock file first

--- src/Ace/Update/Domain/NpmDependencyManager.php
+++ src/Ace/Update/Domain/NpmDependencyManager.php
@@ -30,15 +30,19 @@ class NpmDependencyManager implements DependencyManagerInterface
         // take an md5 sum of lock file before updating
         if (file_exists($lock_file)) {
             $lock_sum = md5_file($lock_file);
+
+            // remove lock file first
+            exec('rm ' . $lock_file);
         }
 
-        exec('npm install', $output, $success);
+        exec('npm update', $output, $success);
         exec('npm shrinkwrap', $output, $success);
 
         // check for changes - return file contents if it's different
         if (md5_file($lock_file) !== $lock_sum){
             return file_get_contents($lock_file);
         }
+
         return null;
     }
 }
